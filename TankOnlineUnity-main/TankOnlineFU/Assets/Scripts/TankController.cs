9f2b6827c05dc7b398cbe238fc6effa120950865
hiuhihi78 <leduchieu2001x@gmail.com>
2023-05-29 00:02:45 +0700

    add source tank enemy auto running

--- TankOnlineUnity-main/TankOnlineFU/Assets/Scripts/TankController.cs
+++ TankOnlineUnity-main/TankOnlineFU/Assets/Scripts/TankController.cs
@@ -34,7 +34,7 @@ public class TankController : MonoBehaviour
 			Position = new Vector3(0, 0, 0),
 			Guid = GUID.Generate()
 		};
-		gameObject.transform.position = _tank.Position;
+		//gameObject.transform.position = _tank.Position;
 		_tankMover = gameObject.GetComponent<TankMover>();
 		//_cameraController = camera.GetComponent<CameraController>();
 		_renderer = gameObject.GetComponent<SpriteRenderer>();
