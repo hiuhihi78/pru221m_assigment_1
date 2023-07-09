da8f05b55c79bf39beedd71b2d1d57a184702e4b
hiuhihi78 <leduchieu2001x@gmail.com>
2023-07-09 11:08:33 +0700

    add random spawn items

---
+++ TankOnlineUnity-main/TankOnlineFU/Assets/Scripts/SpawnItemsController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SpawnItemsController : MonoBehaviour
+    {
+
+        public float minSpawnTime;
+        public float maxSpawnTime;
+
+        public GameObject powerUpTank;
+        public GameObject powerUpShovel;
+        public GameObject powerUpHelmet;
+
+
+        public float topWrap;
+        public float bottomWrap;
+        public float leftWrap;
+        public float rightWrap;
+
+
+        private float randomTimeSpawnItem;
+        private List<GameObject> spawnItems;
+
+        void Start()
+        {
+            randomTimeSpawnItem = GetRandomTimeSpawn() + Time.time;
+            spawnItems = new List<GameObject>()
+            {
+                powerUpHelmet, powerUpTank, powerUpHelmet
+            };
+
+        }
+
+        void Update()
+        {
+            if(Time.time > randomTimeSpawnItem) 
+            {
+                SpawnItem();
+                randomTimeSpawnItem = Time.time + GetRandomTimeSpawn();
+            }
+        }
+
+        void SpawnItem()
+        {
+            Instantiate(GetRandomObjectItem(), GetRandomPositionItem(), Quaternion.identity);
+        }
+
+        private float GetRandomTimeSpawn()
+        {
+            return Random.Range(minSpawnTime, maxSpawnTime); ;
+        }
+
+        private Vector2 GetRandomPositionItem()
+        {
+            Vector2 result = new Vector2();
+            result.x = Random.Range(leftWrap, rightWrap);
+            result.y = Random.Range(bottomWrap, topWrap);
+            return result;
+        }
+
+        private GameObject GetRandomObjectItem()
+        {
+            var random = Random.Range(0, spawnItems.Count);
+            return spawnItems[random];
+        }
+
+    }
+}
